fb94aa105357dca5a04b7a8c9c232724093df922
Usuario <juampimansilla17@gmail.com>
2018-08-03 20:08:43 -0300

    Mi segundo commit

--- src/proyecto/git/ProyectoGit.java
+++ src/proyecto/git/ProyectoGit.java
@@ -16,6 +16,8 @@ public class ProyectoGit {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        
+        System.out.println("Modificacion");
     }
     
 }
